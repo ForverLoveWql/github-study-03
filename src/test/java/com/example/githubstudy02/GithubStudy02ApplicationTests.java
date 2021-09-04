7f314f64b9f4a0b022f602b80d8a88fb5982b517
ForverLoveWql <984958951@qq.com>
2021-09-04 21:04:01 +0800

    "第四次修改"

--- src/test/java/com/example/githubstudy02/GithubStudy02ApplicationTests.java
+++ src/test/java/com/example/githubstudy02/GithubStudy02ApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.githubstudy02;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GithubStudy02ApplicationTests {
+    @Autowired
+    private User user;
 
     @Test
     void contextLoads() {
+        System.out.println(user);
     }
 
 }
