e59298ff8d1ae3a5907f073dced5a3c083191d1d
ForverLoveWql <984958951@qq.com>
2021-09-04 20:26:49 +0800

    第一次提交

---
+++ src/main/java/com/example/githubstudy02/GithubStudy02Application.java
@@ -0,0 +1,15 @@
+package com.example.githubstudy02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubStudy02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubStudy02Application.class, args);
+    }
+
+}
+
+
---
+++ src/main/java/com/example/githubstudy02/dao/UserMapper.java
@@ -0,0 +1,5 @@
+package com.example.githubstudy02.dao;
+
+public interface UserMapper {
+
+}
